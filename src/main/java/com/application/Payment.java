979e663f62cedcfaa48c3980a79cad2efe405f81
praneeth003 <ch.krishnapraneeth@gmail.com>
2025-03-23 13:46:04 -0400

    use jackson data binding to map entries to a list

---
+++ src/main/java/com/application/Payment.java
@@ -0,0 +1,5 @@
+package com.application;
+
+public record Payment(String billingAccount, String monthlyDueDate) {
+
+}
--- src/main/java/com/application/ReminderApp.java
+++ src/main/java/com/application/ReminderApp.java
@@ -1,7 +1,22 @@
 package com.application;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class ReminderApp {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
+        ObjectMapper mapper = new ObjectMapper();
+        try{
+            File file = new File("data.json");
+            List<Payment> paymentList = Arrays.asList(mapper.readValue(file, Payment[].class));
+            System.out.println(paymentList);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
     }
 }
